d6bd465eb6fc8482122631164ec9e116ae26dc08
joseluisdiaz377 <joseluis.diaz377@alu.uhu.es>
2023-10-13 09:58:21 +0200

    test0014 sin funcionar correctamente

--- src/test/CuentaTest.java
+++ src/test/CuentaTest.java
@@ -12,11 +12,15 @@ import pkg.Cuenta;
 
 class CuentaTest {
 	static Cuenta ctaPruebas;
+	static Cuenta cta12345;
+	static Cuenta cta67890;
 	
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 		ctaPruebas = new Cuenta(0);
+		cta12345 = new Cuenta (50);
+		cta67890 = new Cuenta (0);
 	}
 
 	@AfterAll
@@ -50,6 +54,29 @@ class CuentaTest {
 //		assertTrue(ctaPruebas.retirar(3000));  //tiene que dar true porques hay saldo suficiente		
 
 	}
+	
+	@Test
+	void test0014() {
+		cta12345.retirar(200);
+		cta67890.retirar(350);
+		cta12345.ingresar(100);
+		cta67890.retirar(200);
+		cta67890.retirar(150);
+		cta12345.retirar(200);
+		cta67890.ingresar(50);
+		cta67890.retirar(100);
+		
+		System.out.println("Cuenta 12345....");
+		System.out.println("- Saldo Final: " + cta12345.getSaldo());
+		
+		System.out.println("");
+
+		System.out.println("Cuenta 67890....");
+		System.out.println("- Saldo Final: " + cta67890.getSaldo());
+
+	}
+	
+	
 
 	
 
